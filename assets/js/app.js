7fc8fad423ec773001c52e2d07337ef47a3d6bd3
MichelleeLara <99098045+MichelleeLara@users.noreply.github.com>
2022-12-10 00:42:53 -0600

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+// barba.init({
+//     transitions: [{
+//         name: 'prueba',
+//         leave: function(data) {
+//             var done = this.async();
+//             document.body.classList.add("load");
+//             setTimeout(function() {
+//                 done()
+//             }, 900);
+//         },
+//         enter: function(data) {
+//             var done = this.async();
+//             document.body.classList.add("load");
+//             setTimeout(function() {
+//                 done()
+//             }, 900);
+//         }
+//     }]
+// });
